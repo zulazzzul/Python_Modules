7eedc3e4d4a80f57190e5d53a1bb767d2b5f7f50
zulazzzul <nicole.nr.rebelo@gmail.com>
2026-09-26 00:03:49 +0100

    add(py00): ex1

---
+++ py00/ex1/ft_garden_name.py
@@ -0,0 +1,14 @@
+def ft_garden_name():
+    name = input("Enter garden name: ")
+
+    print(f"Garden: {name}")
+    print("Status: Growing Well!")
+
+    # print("Garden:", name) # Por default, separa os parametros com espacos.
+                             # Usa `..., name, sep="<qq coisa>"` para mudar o
+                             # separador para `<qq coisa>`
+
+    # # Outro exemplo:
+    # nome_nini = input("Ola! Como te chamas?? ")
+    # idade = input("Quantos anos tens? ")
+    # print(f"A {nome_nini} tem {idade} anos")
